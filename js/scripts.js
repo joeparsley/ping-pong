ae21ae1b78b02f39f762b04918bd2958a37dec66
Joe Parsley <voodoojoenut@gmail.com>
2017-05-05 11:37:31 -0700

    add countToInput function to take the user input and put it into an array for it to be displayed

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,20 @@
 //BACK-END-LOGIC:
 
+// Takes user input, turnts it into and Array and displays!
+var countToInput = function(input) {
+  var result = [];
+  for (var i = 1; i <= input; i++) {
+    result.push(i);
+  }
+  return result;
+};
 
 
 
-
-
-
+var pingPong = function(input){
+ var output = countToInput(input)
+ return output;
+}
 
 
 
@@ -14,7 +23,7 @@ $(document).ready(function() {
   $('#user-form').submit(function(event) {
     event.preventDefault();
     var input = parseInt($('#number-input').val());
-
-    $('#number-output').text(input);
+    result = pingPong(input);
+    $('#number-output').text(result);
   });
 });
