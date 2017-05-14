bd2c1d28db5425b005a981356e5266800bfe1dd8
Joe Parsley <voodoojoenut@gmail.com>
2017-05-13 18:59:22 -0700

    add pingPong function to put userinput into an array, loop through each element, replacing numbers diveded by 3, 5, 15, with ping, pong and ping-pong

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,28 @@
 //BACK-END-LOGIC:
-
+var pingPong = function(input) {
+  var userArray = [];
+  for (var i = 0; i <= input; i++) {
+    if ((i % 3 === 0) && (i % 5 !== 0)) {
+      userArray.push("ping");
+    } else if ((i % 5 === 0) && (i % 3 !== 0)) {
+      userArray.push("pong");
+    } else if (i % 15 === 0) {
+      userArray.push("ping-pong");
+    } else {
+      userArray.push(" " + i );
+    }
+  }
+  return userArray;
+}
 
 
 //FRONT-END-LOGIC:
 $(document).ready(function() {
   $('#user-form').submit(function(event) {
     event.preventDefault();
+    $("number-output").empty();
     var input = parseInt($('#number-input').val());
     var result = pingPong(input);
-
     result.forEach(function(element) {
       $('#number-output').append( '<li>' + element + '</li>')
     })
